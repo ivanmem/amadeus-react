c1b42730c4d5b9a025805fbcb4805956da1188e7
Ivan Melentyev <ivan.melentev@gost-group.com>
2021-11-12 20:55:20 +0300

    Обновление команд и переход к команде по Enter в поиске

--- src/common/pages/Commands/index.tsx
+++ src/common/pages/Commands/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import {
   Cell,
   Div,
@@ -16,13 +16,33 @@ import { observer } from "mobx-react-lite";
 import { Icon28AddOutline } from "@vkontakte/icons";
 import CommandHelper from "../../helpers/commands/CommandHelper";
 import { commandsStore } from "./common/CommandsStore";
+import { Command } from "../../helpers/commands/types";
 
 const Commands: FC<DefaultPageProps> = () => {
   const router = useRouter();
   const { search, setSearch } = commandsStore;
+
+  const onPushCommand = useCallback(
+    (command: Command) => {
+      router.push({ panel: "command" }, { id: command.id });
+    },
+    [router]
+  );
+
   const searchCommands = useMemo(
     () => commandsService.searchCommand(search),
     [search]
+  );
+
+  const onSearchKeyDown = useCallback(
+    (e: React.KeyboardEvent) => {
+      if (searchCommands.length === 0 || e.key !== "Enter") {
+        return;
+      }
+
+      onPushCommand(searchCommands[0]);
+    },
+    [onPushCommand, searchCommands]
   );
 
   return (
@@ -42,15 +62,15 @@ const Commands: FC<DefaultPageProps> = () => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             after={null}
+            autoFocus
+            onKeyDown={onSearchKeyDown}
           />
           <List>
             {searchCommands.map((command) => (
               <Cell
                 className="alternating-color"
                 key={command.id}
-                onClick={() =>
-                  router.push({ panel: "command" }, { id: command.id })
-                }
+                onClick={() => onPushCommand(command)}
               >
                 {CommandHelper.getCommandFullName(command.id)}
               </Cell>
